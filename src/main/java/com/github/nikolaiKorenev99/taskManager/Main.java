665dca82b381aba4b2908099d4c2d4999fe8391e
nikolaiKorenev99 <nikolaj.korenev99@gmail.com>
2020-11-03 01:31:37 +0200

    Out has been tested

--- src/main/java/com/github/nikolaiKorenev99/taskManager/Main.java
+++ src/main/java/com/github/nikolaiKorenev99/taskManager/Main.java
@@ -3,6 +3,9 @@ package com.github.nikolaiKorenev99.taskManager;
 
 class Main{
     public static void main(String [] args){
-        System.out.println("test");
+        System.out.println("Hello world");
+        System.out.println("Bye");
     }
 }
+
+
